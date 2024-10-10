7c9f0894fba4a0a26c526caed5ec26137d364c7c
Crucible <60628138+CrucibleDev@users.noreply.github.com>
2024-10-10 09:30:28 -0500

    format

--- lua/Crucible/LazyPlugins/telescope.lua
+++ lua/Crucible/LazyPlugins/telescope.lua
@@ -1,34 +1,34 @@
-return{
-	{
-		'nvim-telescope/telescope.nvim',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		},
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
 
-		config = function()
-			require('telescope').setup({})
+        config = function()
+            require('telescope').setup({})
 
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>sf', builtin.find_files, {desc = '[S]earch [F]iles'})
-			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-			vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, {desc = '[S]earch [F]iles'})
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 
-			vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+            vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-			-- Slightly advanced example of overriding default behavior and theme
-			vim.keymap.set('n', '<leader>s/', function()
-				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-				builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-					winblend = 10,
-					previewer = false,
-				})
-			end, { desc = '[/] Fuzzily search in current buffer' })
+            -- Slightly advanced example of overriding default behavior and theme
+            vim.keymap.set('n', '<leader>s/', function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
 
-			vim.keymap.set('n', '<leader>sn', function()
-				builtin.find_files { cwd = vim.fn.stdpath 'config' }
-			end, { desc = '[S]earch [N]eovim files' })
-		end,
-	}
+            vim.keymap.set('n', '<leader>sn', function()
+                builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end, { desc = '[S]earch [N]eovim files' })
+        end,
+    }
 }
 
--- lua/Crucible/LazyPlugins/treesitter.lua
+++ lua/Crucible/LazyPlugins/treesitter.lua
@@ -1,31 +1,31 @@
-return{
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
-		opts = { 
-			ensure_installed = { 
-				'rust',
-				'javascript',
-				'python',
-				'bash',
-				'c',
-				'diff',
-				'html',
-				'lua',
-				'luadoc',
-				'markdown',
-				'markdown_inline',
-				'query',
-				'vim',
-				'vimdoc'
-			},
-			
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			}
-		},
-	}
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = {
+            ensure_installed = {
+                'rust',
+                'javascript',
+                'python',
+                'bash',
+                'c',
+                'diff',
+                'html',
+                'lua',
+                'luadoc',
+                'markdown',
+                'markdown_inline',
+                'query',
+                'vim',
+                'vimdoc'
+            },
+
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            }
+        },
+    }
 }
